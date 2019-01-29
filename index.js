d44aa5136a7a008f0f1ab5276497e46cc33307bb
Lucy Wall <lhwall@gmail.com>
2019-01-29 18:48:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-$(document).ready(function (){
+
+
+
+
+function searchRepositories(){
+const searchTerms = document.getElementById('searchTerms').value
+const searchURL = "https://api.github.com/search/repositories?q=" + searchTerms
+console.log("Search URL is: " + searchURL)
+
+
+$(document).ready(function(){
+$.get('searchURL'), function(response){
+  $("#results").html(response)
+}
+
 });
+debugger
+}
