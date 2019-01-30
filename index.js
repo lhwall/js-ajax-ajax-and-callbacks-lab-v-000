06dd1e75f3095645c5ad68dc6c262ecb59e55a23
Lucy Wall <lhwall@gmail.com>
2019-01-30 15:17:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ $.get(searchURL, function(response){
     let owner = repo.owner.login
     let ownerAvatar = repo.owner.avatar_url
     let ownerLink = repo.owner.html_url
-return `<li><h2><a href='${url}'>${name}</a></h2><br>${description}<br><strong><a href='{ownerLink}'>${owner} <img src='${ownerAvatar}'></a></li>`
+return `<li><h2><a href='${url}'>${name}</a></h2><br>${description}<br><strong><a href='{ownerLink}'>${owner} <img src='${ownerAvatar}' height='20'></a></li>`
   }).join("")
   + "</ul>"
   $("#results").html(reposHTML)
