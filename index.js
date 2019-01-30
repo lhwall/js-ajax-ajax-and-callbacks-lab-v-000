744af8e78930f5853d25e32c116f96a75e629eed
Lucy Wall <lhwall@gmail.com>
2019-01-30 15:17:27 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,25 @@ console.log("Search URL is: " + searchURL)
 
 $(document).ready(function(){
 $.get(searchURL, function(response){
-  $("#results").html(response)
+  console.log(Object.keys(response.items[0].owner))
+  let reposHTML = "<ul>" +
+  response.items.map(
+    repo =>{
+    let name = repo.name
+    let description = repo.description
+    let url = repo.html_url
+    let owner = repo.owner.login
+    let ownerAvatar = repo.owner.avatar_url
+    let ownerLink = repo.owner.html_url
+return `<li><h2><a href='${url}'>${name}</a></h2><br>${description}<br><strong><a href='{ownerLink}'>${owner} <img src='${ownerAvatar}'></a></li>`
+  }).join("")
+  + "</ul>"
+  $("#results").html(reposHTML)
+
 })
 });
 
 
 
-debugger
+
 }
