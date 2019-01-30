eb70f17b0605abd0da4f9d8fe84f692386590ab8
Lucy Wall <lhwall@gmail.com>
2019-01-30 19:50:22 +0000

    Done.

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ $.get(searchURL, function(respond){
     let owner = repo.owner.login
     let ownerAvatar = repo.owner.avatar_url
     let ownerLink = repo.owner.html_url
-return `<li><h3><a href='${url}'>${name}</a></h3>${description}<br><strong><a href='{ownerLink}'>${owner} <img src='${ownerAvatar}' height='20'></a><a href="#" onclick="showCommits(this)">Show Commits</a></li>`
+return `<li><h3><a href='${url}'>${name}</a></h3>${description}<br><strong><a href='{ownerLink}'>${owner} <img src='${ownerAvatar}' height='20'></a><a href="#" onclick="showCommits(this)" data-owner=${owner} data-repository="${name}">Show Commits</a></li> `
   }).join("")
   + "</ul>"
 
@@ -30,6 +30,26 @@ return `<li><h3><a href='${url}'>${name}</a></h3>${description}<br><strong><a hr
 });
 }
 
-function showCommits(){
+function showCommits(el){
+  repository = el.dataset.repository
+  repositoryOwner = el.dataset.owner
+  debugger
+  let getCommitsURL = "https://api.github.com/" + repositoryOwner + "/" + repository + "/commits"
+  $(document).ready(function(){
+  $.get(getCommitsURL, function(respond){
+    console.log(Object.keys(respond.items[0].owner))
+    let commitsHTML = "<ul>" +
+    respond.items.map(
+      commit =>{
+      let sha = commit.sha
+      let authorName = commit.author.name
+      let authorLogin = commit.author.login
+      let authorAvatar = commit.author.avatar_url
+  return `<li><strong>${sha} ${authorName} <img src='${authorAvatar}' height='20'> ${authorLogin}</li>`
+    }).join("")
+    + "</ul>"
 
-}
+    document.getElementById("details").innerHTML = commitsHTML
+  })
+  });
+  }
